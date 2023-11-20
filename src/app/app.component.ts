710647e4da14d30761cd411bfe78267185ca88ce
Julian Moreno <julian.m.personal@gmail.com>
2023-11-20 13:13:00 -0300

    Se sube ultima actualizacion.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { LocationService } from './services/location-service/location.service';
-import { WheaterService } from './services/weather-service/wheater.service';
 
 @Component({
   selector: 'app-root',
@@ -10,50 +8,4 @@ import { WheaterService } from './services/weather-service/wheater.service';
 
 export class AppComponent {
   title = 'Wheater App';
-  climate: number = 0;
-  weather: string = '';
-  latAndLong: string = '';
-
-  climates= [18, 24, 30];
-  weathers = ['soleado', 'lluvioso', 'nublado'];
-
-  public latitude: any;
-  public longitude: any;
-
-  public response: any;
-
-  constructor(public locationService: LocationService, public wheaterService: WheaterService) {}
-
-  ngOnInit() {
-    this.getLocation();
-  }
-
-  getData() {
-    // Json de prueba 
-    this.response = {
-      localizedName: 'Luis Guillon',
-      city: 'Buenos Aires',
-      country: 'Argentina',
-      weatherInMetric: '25°C',
-      isDayTime: true,
-      weatherText: 'Partly sunny'
-    }; 
-    
-    /*this.wheaterService.getData(this.latitude, this.longitude).subscribe(
-      (response: any) => {
-        this.response = response;
-      },
-      (error: any) => {
-        console.error(error);
-      }
-    );*/
-  }
-
-  getLocation() {
-    this.locationService.getPosition().then(pos => {
-      this.latitude = pos.lat;
-      this.longitude = pos.lng;
-      this.getData();
-    });
-  }
 }
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { LocationService } from '../services/location-service/location.service';
+import { WheaterService } from '../services/weather-service/wheater.service';
 
 @Component({
   selector: 'app-menu',
@@ -7,21 +9,50 @@ import { Component } from '@angular/core';
 })
 
 export class MenuComponent {
+  climate: number = 0;
+  weather: string = '';
+  latAndLong: string = '';
+
+  climates= [18, 24, 30];
+  weathers = ['soleado', 'lluvioso', 'nublado'];
+
+  public latitude: any;
+  public longitude: any;
+
   public response: any;
 
+  constructor(public locationService: LocationService, public wheaterService: WheaterService) {}
+
   ngOnInit() {
-    this.getData();
+    this.getLocation();
   }
 
   getData() {
     // Json de prueba 
-    this.response = {
+    /*this.response = {
       localizedName: 'Luis Guillon',
       city: 'Buenos Aires',
       country: 'Argentina',
       weatherInMetric: '25°C',
       isDayTime: true,
       weatherText: 'Partly sunny'
-    };
+    }; */
+    
+    this.wheaterService.getData(this.latitude, this.longitude).subscribe(
+      (response: any) => {
+        this.response = response;
+      },
+      (error: any) => {
+        console.error(error);
+      }
+    );
+  }
+
+  getLocation() {
+    this.locationService.getPosition().then(pos => {
+      this.latitude = pos.lat;
+      this.longitude = pos.lng;
+      this.getData();
+    });
   }
 }
